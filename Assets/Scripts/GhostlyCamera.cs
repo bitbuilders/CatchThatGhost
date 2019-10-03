4611a1b5f69307ecd3d4da4802ad3b544c542449
Colin Misbach <colin@misbach.com>
2019-10-03 01:25:51 -0600

    Walking! :penguin:

---
+++ Assets/Scripts/GhostlyCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostlyCamera : MonoBehaviour
+{
+    [SerializeField] GameObject m_trackee = null;
+
+    void LateUpdate()
+    {
+        transform.position = m_trackee.transform.position + Vector3.back * 10.0f;
+    }
+}
--- Assets/Scripts/GhostlyController.cs
+++ Assets/Scripts/GhostlyController.cs
@@ -6,11 +6,16 @@ public class GhostlyController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D m_rigidbody = null;
 
+    [SerializeField] Animator m_animator = null;
+
     [Tooltip("How much force can be applied to the Rigidbody when at top speed")]
     [SerializeField, Range(0.0f, 200.0f)] float m_maxForce = 100.0f;
 
     [Tooltip("How fast will you reach top speed? (1.0 = 1 second, 2.0 = 0.5 seconds)")]
     [SerializeField, Range(0.0f, 20.0f)] float m_acceleration = 3.0f;
+
+    [Tooltip("How fast is considered walking at regular speed?")]
+    [SerializeField, Range(0.0f, 20.0f)] float m_walkThreshold = 5.1f;
 
     Vector2 m_force = Vector2.zero;
     Vector2 m_input = Vector2.zero;
@@ -22,7 +27,16 @@ public class GhostlyController : MonoBehaviour
 
     void Update()
     {
-        
+        bool walking = m_force.sqrMagnitude > 5.0f;
+        m_animator.SetBool("Walking", walking);
+        m_animator.SetFloat("WalkSpeed", m_rigidbody.velocity.sqrMagnitude / (m_walkThreshold * m_walkThreshold));
+
+        if (walking)
+        {
+            Vector2 dir = m_force.normalized;
+            m_animator.SetFloat("xDir", dir.x);
+            m_animator.SetFloat("yDir", dir.y);
+        }
     }
 
     private void FixedUpdate()
